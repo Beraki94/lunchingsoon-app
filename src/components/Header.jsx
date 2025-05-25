194171df6ad20bbbaa493559706ae56aca242a5d
Beraki Digital <berakidigital@gmail.com>
2025-05-25 23:50:23 +0100

    loading

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ export default function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Joe and Joe Bags</h1>
+      <p className="header__subtitle"><em>Since 1998</em></p>
     </header>
   );
 }
